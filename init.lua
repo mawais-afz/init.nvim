457319b71333b314f997ca6ece3a31f8a488e39f
Muhammad Awais <mawais.afz@gmail.com>
2025-05-09 07:22:06 +0500

    Update

--- init.lua
+++ init.lua
@@ -13,7 +13,31 @@ end ---@diagnostic disable-next-line: undefined-field
 vim.opt.rtp:prepend(lazypath)
 
 require('lazy').setup({
+  -- themes
+  require('plugins.nord'),
   require('plugins.onedark'),
-  require('plugins.onedark'),
-
+  
+  -- plugins
+  -- require('plugins.alpha'),
+  -- require('plugins.aerial'),
+  -- require('plugins.autocompletion'),
+  -- require('plugins.avante'),
+  -- require('plugins.bufferline'),
+  -- require('plugins.chatgpt'),
+  -- require('plugins.comment'),
+  -- require('plugins.database'),
+  -- require('plugins.debug'),
+  -- require('plugins.gitsigns'),
+  -- require('plugins.harpoon'),
+  -- require('plugins.indent-blankline'),
+  -- require('plugins.lazygit'),
+  -- require('plugins.lsp'),
+  -- require('plugins.lualine'),
+  -- require('plugins.misc'),
+  -- require('plugins.neo-tree'),
+  -- require('plugins.none-ls'),
+  
+  -- require('plugins.telescope'),
+  -- require('plugins.treesitter'),
+  -- require('plugins.vim-tmux-navigator'),
 })
