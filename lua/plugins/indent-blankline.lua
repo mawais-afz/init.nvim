d5aae058438bf871a1f14891c96f8fca133ce584
Muhammad Awais <mawais.afz@gmail.com>
2025-05-09 09:35:45 +0500

    Update

--- lua/plugins/indent-blankline.lua
+++ lua/plugins/indent-blankline.lua
@@ -1,47 +1,33 @@
 return {
-  {
-    'lukas-reineke/indent-blankline.nvim',
-    main = 'ibl',
-    opts = {
-      indent = {
-        char = '▏',
-        highlight = {
-          "IndentBlanklineIndent1",
-          "IndentBlanklineIndent2",
-          "IndentBlanklineIndent3",
-          "IndentBlanklineIndent4",
-          "IndentBlanklineIndent5",
-          "IndentBlanklineIndent6",
-        },
-      },
-      scope = {
-        show_start = false,
-        show_end = false,
-        show_exact_scope = false,
-        highlight = {
-          "IndentBlanklineScopeChar",
-          "IndentBlanklineScopeStart",
-          "IndentBlanklineScopeEnd",
-        },
-      },
-      exclude = {
-        filetypes = {
-          "Trouble",
-          "alpha",
-          "dashboard",
-          "help",
-          "lazy",
-          "mason",
-          "neo-tree",
-          "notify",
-          "snacks_dashboard",
-          "snacks_notif",
-          "snacks_terminal",
-          "snacks_win",
-          "toggleterm",
-          "trouble",
-        },
+  'lukas-reineke/indent-blankline.nvim',
+  main = 'ibl',
+  opts = {
+    indent = {
+      char = "│",
+      tab_char = "│",
+    },
+    scope = {
+      show_start = false,
+      show_end = false,
+      show_exact_scope = false,
+    },
+    exclude = {
+      filetypes = {
+        "Trouble",
+        "alpha",
+        "dashboard",
+        "help",
+        "lazy",
+        "mason",
+        "neo-tree",
+        "notify",
+        "snacks_dashboard",
+        "snacks_notif",
+        "snacks_terminal",
+        "snacks_win",
+        "toggleterm",
+        "trouble",
       },
     },
-  }
+  },
 }
